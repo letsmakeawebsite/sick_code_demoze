d6f85e404b50b26ca1df72cc5801d53151ec5f98
kevinjdolan <norelaxation@gmail.com>
2017-03-23 19:01:52 -0400

    a new template for all

--- app.py
+++ app.py
@@ -117,7 +117,7 @@ def renderWebsite(website):
     data = yaml.safe_load(website.data)
     data['domain'] = website.domain
     return flask.render_template(
-        'bootstrap-marketing/index.html',
+        '%s/index.html' % website.template,
         **data
     )
 
@@ -161,6 +161,76 @@ feature4Title:
 feature4Text:
 """.strip()
 
+BOOSTRAP_STARTUP_SRC = """
+title: "Somebody Sells Something"
+shortTitle: "Somebody's Somethings"
+headerImage: ""
+headline: ""
+tagline: ""
+buttonText: ""
+
+enableSection2: false
+section2Image: ""
+section2Title: "Social Local Mobile"
+section2Heading: ""
+section2Description: ""
+section2Feature1Heading: "Quick Views"
+section2Feature1Description: ""
+section2Feature2Heading: "Detailed Drilldowns"
+section2Feature2Description: ""
+
+enableDevSection: false
+devSectionTitle: "Optimized For Developers"
+devSectionHeading: "Developers run the world."
+devSectionBody: ""
+devSectionButton: "Read Our Docs"
+devSectionCode: |
+  LABEL START
+  PUT "U" >> STDOUT
+  PUT "R" >> STDOUT
+  PUT " " >> STDOUT
+  PUT "D" >> STDOUT
+  PUT "U" >> STDOUT
+  PUT "M" >> STDOUT
+  PUT "B" >> STDOUT
+  GO TO START
+
+enableQuote: false
+quoteImage:
+quoteTitle: "Don't take our word for it"
+quoteBody: ""
+quoteAttribution: "Name, Job"
+
+enableFeatures: false
+featureTitle: ""
+featureBody: ""
+feature1Header: "Mobile Optimized"
+feature1Body: ""
+feature2Header: "Save Money"
+feature2Body: ""
+feature3Header: "Save Time"
+feature3Body: ""
+enableFeaturesLine2: true
+feature4Header: "Complex Calculations"
+feature4Body: ""
+feature5Header: "24/7 Support"
+feature5Body: ""
+feature6Header: "Kid Tested, Mother Approved"
+feature6Body: ""
+
+enablePlans: false
+plansTitle: Flexible Plans
+planUnits: "Monthly Allowance"
+plansBody: "We don't believe that one size fits all, so we have 3 plans to fit your needs best!"
+plan1: Personal
+plan1Body: "You're just a person. Your vote doesn't count. We don't care about you."
+plan2: Business
+plan2Body: "Business runs the world, but not all business is big business!"
+plan3: Corporate
+plan3Body: "Corporations run the world, and you're a big-wig. This one's for you!"
+""".strip()
+
 DEFAULT_SRCS = {
     'bootstrap-marketing': BOOTSTRAP_MARKETING_SRC,
+    'bootstrap-startup': BOOSTRAP_STARTUP_SRC,
 }
